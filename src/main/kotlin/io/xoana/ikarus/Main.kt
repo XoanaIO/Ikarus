a62ef8ab4cf00e8bb6f670d1f4f18f14285b8b6d
Joseph Catrambone <jo.jcat@gmail.com>
2017-05-21 11:39:04 -0700

    Fixing some issues with Node basis.  Generic types now.

--- src/main/kotlin/io/xoana/ikarus/Main.kt
+++ src/main/kotlin/io/xoana/ikarus/Main.kt
@@ -7,6 +7,6 @@ import io.xoana.ikarus.nodes.*;
 
 fun main(args: Array<String>) {
 	println("Hello, world!");
-	val inp = InputNode(10, 10)
-	val n = UnaryNode((x:Double) -> { x*x }, (x:Double) -> { 2.0*x }, inp)
+	//val inp = InputNode(10, 10)
+	//val n = UnaryNode((x:Double) -> { x*x }, (x:Double) -> { 2.0*x }, inp)
 }
--- src/main/kotlin/io/xoana/ikarus/nodes/Node.kt
+++ src/main/kotlin/io/xoana/ikarus/nodes/Node.kt
@@ -1,33 +1,34 @@
 package io.xoana.ikarus.nodes;
 
 import koma.*;
-import koma.matrix.Matrix;
+import koma.matrix.*
 
-abstract class Node(val rows:Int, val columns:Int, val inputs:Array<Node>) {
-	abstract fun forward(args:Array<Matrix>): Matrix;
-	abstract fun reverse(forward: Array<Matrix>, adjoint: Matrix): Array<Matrix>;
+abstract class Node<T : Number>(val rows:Int, val columns:Int, val inputs:Array<Node<T>>) {
+	abstract fun forward(args:Array<Matrix<T>>): Matrix<T>;
+	abstract fun reverse(forward: Array<Matrix<T>>, adjoint: Matrix<T>): Array<Matrix<T>>;
 }
 
-class InputNode(rows, columns) : Node(rows, columns, arrayOf()) {
-	override fun forward(args:Array<Matrix>): Matrix {
-		return args[0]!!
+class InputNode<T:Number>(rows:Int, columns:Int) : Node<T>(rows, columns, arrayOf()) {
+	override fun forward(args:Array<Matrix<T>>): Matrix<T> {
+		return args[0]
 	}
 
-	override fun reverse(forward: Array<Matrix>, adjoint: Matrix): Array<Matrix> {
+	override fun reverse(forward: Array<Matrix<T>>, adjoint: Matrix<T>): Array<Matrix<T>> {
 		return arrayOf()
 	}
 }
 
-class UnaryNode(input: Node, f: (Double)->Double, df: (Double)->Double) : Node(input.rows, input.columns, arrayOf(input)) {
+class UnaryNode<T:Number>(input: Node<T>, val f: (T)->T, val df: (T)->T) : Node<T>(input.rows, input.columns, arrayOf(input)) {
 	// Forward: z := f(x)
 	// adj_x += (adj_z dot df(x))
-	override fun forward(args:Array<Matrix>): Matrix {
-		return x.mapMat { f(it) }
+	override fun forward(args:Array<Matrix<T>>): Matrix<T> {
+		return args[0].map { f(it) }
 	}
 
-	override fun reverse(forward: Array<Matrix>, adjoint: Matrix): Array<Matrix> {
-		return forward.forEach( fwd -> {
-			fwd.mapMat { df(it) } emul adjoint
-		})
+	override fun reverse(forward: Array<Matrix<T>>, adjoint: Matrix<T>): Array< Matrix<T> > {
+		return forward.map{ fwd -> {
+			fwd.map(df).elementTimes(adjoint)
+		}.invoke() }.toTypedArray()
+			//fwd.mapMat { df(it) } emul adjoint
 	}
 }
